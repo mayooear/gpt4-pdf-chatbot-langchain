28ee43224547424952e752e87e94758ac4e027e8
mrcruz117 <mrcruz1989@gmail.com>
2023-03-27 16:53:59 -0400

    env changes

--- config/pinecone.ts
+++ config/pinecone.ts
@@ -2,7 +2,7 @@
  * Change the index and namespace to your own
  */
 
-const PINECONE_INDEX_NAME = 'langchainjsfundamentals';
+const PINECONE_INDEX_NAME = 'langchain-index';
 
 const PINECONE_NAME_SPACE = 'pdf-test'; //namespace is optional for your vectors
 
