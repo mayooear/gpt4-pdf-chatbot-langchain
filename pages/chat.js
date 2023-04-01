588c668becd5fc1c6ea1040ddd65ab5dcd66adf9
KiwiKid <KiwiKid@users.noreply.github.com>
2023-04-01 16:35:54 +1300

    make chat.js a dynamic import

--- pages/chat.js
+++ pages/chat.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import Chat from '@/components/chat';
 import Layout from '@/components/layout';
+
+import dynamic from 'next/dynamic'
+
+const DynamicChat = dynamic(() => import('@/components/chat'), {
+  loading: () => <p>Loading...</p>,
+})
 
 const ChatPage = (props) => {
   console.log('props', props);
 
   return (
     <Layout>
-      <Chat />
+      <DynamicChat />
     </Layout>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import { useRef, useState, useEffect, useMemo, useCallback } from 'react';
 import Layout from '@/components/layout';
 import styles from '@/styles/Home.module.css';
 import { Message } from '@/types/chat';
-import { fetchEventSource } from '@microsoft/fetch-event-source';
 import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
 import LoadingDots from '@/components/ui/LoadingDots';
