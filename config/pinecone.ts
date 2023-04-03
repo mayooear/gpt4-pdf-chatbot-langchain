2a4c12788828aa2917c1951707684aa4ff2e0776
sw5park <47099787+sw5park@users.noreply.github.com>
2023-04-03 02:30:18 +0000

    good

--- config/pinecone.ts
+++ config/pinecone.ts
@@ -3,11 +3,11 @@
  */
 
 if (!process.env.PINECONE_INDEX_NAME) {
-  throw new Error('Missing Pinecone index name in .env file');
+  throw new Error('Missing Pinecone index name in .env file..');
 }
 
-const PINECONE_INDEX_NAME = process.env.PINECONE_INDEX_NAME ?? '';
+const PINECONE_INDEX_NAME = 'demo';
 
-const PINECONE_NAME_SPACE = 'pdf-test'; //namespace is optional for your vectors
+const PINECONE_NAME_SPACE = 'pdftest'; //namespace is optional for your vectors
 
 export { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE };
