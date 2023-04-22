3e20dbd7be58d8986f6572c8c01f60ac3a9742b7
kai <luoshen.data@gmail.com>
2023-04-22 11:53:19 -0700

    save

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -9,19 +9,24 @@ Chat History:
 Follow Up Input: {question}
 Standalone question:`;
 
-const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
+const QA_PROMPT = `You are a world-class sales agent. 
+Clients come to you to ask questions about Turing.com, 
+and your goal is to motivate clients to book a call with a real sales agent after a few questions.
+Use the following pieces of context to answer the question at the end. Keep the answer well-formated and concise.
 If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
-If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
+If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context. 
+The client can always book a call at https://www.turing.com/teams
 
 {context}
 
 Question: {question}
 Helpful answer in markdown:`;
 
+// can change back to gpt-3.5-turbo
 export const makeChain = (vectorstore: PineconeStore) => {
   const model = new OpenAI({
     temperature: 0, // increase temepreature to get more creative answers
-    modelName: 'gpt-3.5-turbo', //change this to gpt-4 if you have access
+    modelName: 'gpt-4', //change this to gpt-4 if you have access
   });
 
   const chain = ConversationalRetrievalQAChain.fromLLM(
@@ -30,7 +35,7 @@ export const makeChain = (vectorstore: PineconeStore) => {
     {
       qaTemplate: QA_PROMPT,
       questionGeneratorTemplate: CONDENSE_PROMPT,
-      returnSourceDocuments: true, //The number of source documents returned is 4 by default
+      returnSourceDocuments: false, //The number of source documents returned is 4 by default
     },
   );
   return chain;
