ff2880ed6841b0ca81a2ecf1d6aa242ed5dd982d
Mayo <mijooear@gmail.com>
2023-04-27 03:10:23 +0100

    vectordbqachain

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -1,37 +1,45 @@
 import { OpenAI } from 'langchain/llms/openai';
 import { PineconeStore } from 'langchain/vectorstores/pinecone';
-import { ConversationalRetrievalQAChain } from 'langchain/chains';
+import { LLMChain, loadQAChain, ChatVectorDBQAChain } from 'langchain/chains';
+import { PromptTemplate } from 'langchain/prompts';
 
-const CONDENSE_PROMPT = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+const CONDENSE_PROMPT =
+  PromptTemplate.fromTemplate(`Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
 
 Chat History:
 {chat_history}
 Follow Up Input: {question}
-Standalone question:`;
+Standalone question:`);
 
-const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
+const QA_PROMPT =
+  PromptTemplate.fromTemplate(`You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
 If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
 If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
 
 {context}
 
 Question: {question}
-Helpful answer in markdown:`;
+Helpful answer in markdown:`);
 
 export const makeChain = (vectorstore: PineconeStore) => {
-  const model = new OpenAI({
-    temperature: 0, // increase temepreature to get more creative answers
-    modelName: 'gpt-3.5-turbo', //change this to gpt-4 if you have access
+  const questionGenerator = new LLMChain({
+    llm: new OpenAI({ temperature: 0 }),
+    prompt: CONDENSE_PROMPT,
   });
 
-  const chain = ConversationalRetrievalQAChain.fromLLM(
-    model,
-    vectorstore.asRetriever(),
+  const docChain = loadQAChain(
+    //change modelName to gpt-4 if you have access to it
+    new OpenAI({ temperature: 0, modelName: 'gpt-3.5-turbo' }),
     {
-      qaTemplate: QA_PROMPT,
-      questionGeneratorTemplate: CONDENSE_PROMPT,
-      returnSourceDocuments: true, //The number of source documents returned is 4 by default
+      prompt: QA_PROMPT,
     },
   );
-  return chain;
+
+  return new ChatVectorDBQAChain({
+    vectorstore,
+    combineDocumentsChain: docChain,
+    questionGeneratorChain: questionGenerator,
+    returnSourceDocuments: true,
+    k: 4, //number of source documents to return. Change this figure as required.
+  });
 };
