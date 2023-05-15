8edf74c3e261a70b8a495176b89bcd01a4d6e027
Odhran7 <russell.odhran@gmail.com>
2023-05-15 22:35:42 +0100

    updates

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,8 +7,9 @@ export default function Layout({ children }: LayoutProps) {
     <div className="mx-auto flex flex-col space-y-4">
       <header className="container sticky top-0 z-40 bg-white">
         <div className="h-16 border-b border-b-slate-200 py-4">
-          <nav className="ml-4 pl-6">
+          <nav className="ml-4 pl-6 flex items-center">
             <a href="#" className="hover:text-slate-600 cursor-pointer">
+              <img src="/logo.png" alt="Logo" className="h-10 w-25 mr-3" /> {/* Adjust height, width and margin as needed */}
               Home
             </a>
           </nav>
--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home() {
   }>({
     messages: [
       {
-        message: 'Hi, what would you like to learn about this legal case?',
+        message: 'Hi, what would you like to learn about Apple?',
         type: 'apiMessage',
       },
     ],
@@ -125,7 +125,7 @@ export default function Home() {
       <Layout>
         <div className="mx-auto flex flex-col gap-4">
           <h1 className="text-2xl font-bold leading-[1.1] tracking-tighter text-center">
-            Chat With Your Legal Docs
+            Valumetrics
           </h1>
           <main className={styles.main}>
             <div className={styles.cloud}>
@@ -137,7 +137,7 @@ export default function Home() {
                     icon = (
                       <Image
                         key={index}
-                        src="/bot-image.png"
+                        src="/VM.jpg"
                         alt="AI"
                         width="40"
                         height="40"
@@ -224,7 +224,7 @@ export default function Home() {
                     placeholder={
                       loading
                         ? 'Waiting for response...'
-                        : 'What is this legal case about?'
+                        : 'Tell me about Apple\'s dividend?'
                     }
                     value={query}
                     onChange={(e) => setQuery(e.target.value)}
