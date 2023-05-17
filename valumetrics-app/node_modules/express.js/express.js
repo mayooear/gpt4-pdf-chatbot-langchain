7a7b4fa91f3cf8f955f149e35f29f1bffc57a05f
Odhran Russell <65116550+Odhran7@users.noreply.github.com>
2023-05-17 14:35:31 +0100

    Delete valumetrics-app directory

--- valumetrics-app/node_modules/express.js/express.js
+++
@@ -1,8 +0,0 @@
-var express = require('express')
-var app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3000)
--- valumetrics-app/node_modules/express.js/test.js
+++
@@ -1,5 +0,0 @@
-var x=10;
-var y=20;
-x+y;
-var sum=_
-console.log(sum)
