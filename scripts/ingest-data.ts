09430ceb88ca8c57b28422ad010b84773d4c0e0c
asipiome <asipiome@students.zhaw.ch>
2023-05-30 20:42:15 +0200

    -disabled ingest data for pinecone to prevent redundant sources of data in db

--- scripts/ingest-data.ts
+++ scripts/ingest-data.ts
@@ -36,11 +36,11 @@ export const run = async () => {
     const index = pinecone.Index(PINECONE_INDEX_NAME); //change to your own index name
 
     //embed the PDF documents
-    await PineconeStore.fromDocuments(docs, embeddings, {
-      pineconeIndex: index,
-      namespace: PINECONE_NAME_SPACE,
-      textKey: 'text',
-    });
+    // await PineconeStore.fromDocuments(docs, embeddings, {
+    //   pineconeIndex: index,
+    //   namespace: PINECONE_NAME_SPACE,
+    //   textKey: 'text',
+    // });
   } catch (error) {
     console.log('error', error);
     throw new Error('Failed to ingest your data');
