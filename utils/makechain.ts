12caae7ed3091a32bd668a08826f8f12fa6bb43e
ScottBerenzweig <76454555+betterbrand@users.noreply.github.com>
2023-06-18 20:12:55 +0000

    Pending changes exported from your codespace

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -21,7 +21,7 @@ Helpful answer in markdown:`;
 export const makeChain = (vectorstore: PineconeStore) => {
   const model = new OpenAI({
     temperature: 0, // increase temepreature to get more creative answers
-    modelName: 'gpt-3.5-turbo', //change this to gpt-4 if you have access
+    modelName: 'gpt-4', //change this to gpt-4 if you have access
   });
 
   const chain = ConversationalRetrievalQAChain.fromLLM(
