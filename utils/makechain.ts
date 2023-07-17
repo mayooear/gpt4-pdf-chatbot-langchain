d79ea350ec0e5772e88691ba4464715aa43f9247
maxlu_lat <max.lu@latimes.com>
2023-07-17 17:29:41 -0400

    minor change to temp and returned doc liimt

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -9,7 +9,7 @@ Chat History:
 Follow Up Input: {question}
 Standalone question:`;
 
-const QA_PROMPT = `You are a helpful AI-writing assistant. Use the following pieces of context to answer the question at the end.
+const QA_PROMPT = `You are a writer-pro GPT and you are helping me with writing an article that discuss the historic rise of NUS among global universities. Use the following pieces of context (transcripts of interviews with people from NUS) to answer the question at the end.
 If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
 If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
 
@@ -20,7 +20,7 @@ Helpful answer in markdown:`;
 
 export const makeChain = (vectorstore: PineconeStore) => {
   const model = new OpenAI({
-    temperature: 0, // increase temepreature to get more creative answers
+    temperature: 0.3, // increase temepreature to get more creative answers
     modelName: 'gpt-4', //change this to gpt-4 if you have access
   });
 
