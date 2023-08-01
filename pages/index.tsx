1b34a5a03bed5291a3c7b692fb6df40223a1e6ed
YimmyYohnson <Jethro.reeve@gmail.com>
2023-08-01 13:34:26 +0100

    Updated footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -261,8 +261,8 @@ export default function Home() {
           </main>
         </div>
         <footer className="m-auto p-4">
-          <a href="https://twitter.com/mayowaoshin">
-            Powered by LangChainAI. Demo built by Mayo (Twitter: @mayowaoshin).
+          <a href="https://github.com/YimmyYohnson/gpt4-pdf-chatbot-langchain-musicneuro-chat">
+            Powered by LangChainAI. Version available on GitHub
           </a>
         </footer>
       </Layout>
