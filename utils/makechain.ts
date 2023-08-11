ba9b6630d0966adbd020f415adbb41c9b2e9bf41
jacoblee93 <jacoblee93@gmail.com>
2023-08-10 17:21:49 -0700

    Update deps, naming

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -2,14 +2,14 @@ import { ChatOpenAI } from 'langchain/chat_models/openai';
 import { PineconeStore } from 'langchain/vectorstores/pinecone';
 import { ConversationalRetrievalQAChain } from 'langchain/chains';
 
-const CONDENSE_PROMPT = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+const CONDENSE_TEMPLATE = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
 
 Chat History:
 {chat_history}
 Follow Up Input: {question}
 Standalone question:`;
 
-const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
+const QA_TEMPLATE = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
 If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
 If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
 
@@ -28,8 +28,8 @@ export const makeChain = (vectorstore: PineconeStore) => {
     model,
     vectorstore.asRetriever(),
     {
-      qaTemplate: QA_PROMPT,
-      questionGeneratorTemplate: CONDENSE_PROMPT,
+      qaTemplate: QA_TEMPLATE,
+      questionGeneratorTemplate: CONDENSE_TEMPLATE,
       returnSourceDocuments: true, //The number of source documents returned is 4 by default
     },
   );
