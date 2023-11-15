ab83826f5d2f27bb944f115987ac6036a1913f66
Michael Olivier <molivier@baac.net>
2023-11-14 16:12:58 -0800

    copy edits

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home() {
   }>({
     messages: [
       {
-        message: 'Hi, what would you like to learn about this document?',
+        message: 'Hi, what would you like to learn about from the Ananda Library?',
         type: 'apiMessage',
       },
     ],
@@ -125,7 +125,7 @@ export default function Home() {
       <Layout>
         <div className="mx-auto flex flex-col gap-4">
           <h1 className="text-2xl font-bold leading-[1.1] tracking-tighter text-center">
-            Chat With Your Docs
+            Chat With the Ananda Librarian!
           </h1>
           <main className={styles.main}>
             <div className={styles.cloud}>
@@ -267,9 +267,7 @@ export default function Home() {
           </main>
         </div>
         <footer className="m-auto p-4">
-          <a href="https://twitter.com/mayowaoshin">
-            Powered by LangChainAI. Demo built by Mayo (Twitter: @mayowaoshin).
-          </a>
+          Powered by LangChainAI and gpt4-pdf-chatbot-langchain open source projects.
         </footer>
       </Layout>
     </>
