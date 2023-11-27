3c58fdf818a43fb40ada9546005450dcd973c7f5
Paul Bryan <paul@fghdev.com>
2023-11-27 16:25:46 +0800

    fix compile error

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -1,4 +1,4 @@
-import {AzureChatOpenAI, ChatOpenAI } from 'langchain/chat_models/openai';
+import { ChatOpenAI } from 'langchain/chat_models/openai';
 import { ChatPromptTemplate } from 'langchain/prompts';
 import { RunnableSequence } from 'langchain/schema/runnable';
 import { StringOutputParser } from 'langchain/schema/output_parser';
