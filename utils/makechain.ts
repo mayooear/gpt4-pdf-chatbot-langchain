34289f827faa74e30e77727c6d8a402e846f0d6f
Paul Bryan <paul@fghdev.com>
2023-11-28 10:53:36 +0800

    add prompts to environment

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -5,7 +5,9 @@ import { StringOutputParser } from 'langchain/schema/output_parser';
 import type { Document } from 'langchain/document';
 import type { VectorStoreRetriever } from 'langchain/vectorstores/base';
 
-const CONDENSE_TEMPLATE = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+const CONDENSE_TEMPLATE =
+  process.env.NEXT_PUBLIC_CONDENSE_TEMPLATE ??
+  `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
 
 <chat_history>
   {chat_history}
@@ -14,7 +16,7 @@ const CONDENSE_TEMPLATE = `Given the following conversation and a follow up ques
 Follow Up Input: {question}
 Standalone question:`;
 
-const QA_TEMPLATE = `You are human resources expert. Use the following pieces of context to answer the question at the end.
+const QA_TEMPLATE = process.env.NEXT_PUBLIC_QA_TEMPLATE ?? `You are human resources expert. Use the following pieces of context to answer the question at the end.
 If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
 If the question is not related to the context or chat history, politely respond that you are tuned to only answer questions that are related to the context.
 
