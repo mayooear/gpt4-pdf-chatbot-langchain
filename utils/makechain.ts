001c192a9c180cee6051263fdbdd46b0c194d040
Michael Olivier <molivier@baac.net>
2024-02-15 14:54:35 -0800

    System, prompt, improvements, and switch to GPT's turbo for preview, which seems to be different than 1106. For one thing, with this updated system prompt it seems better than 1106 at refusing to answer a generic query like tell me about disco

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -10,12 +10,18 @@ Follow Up Input: {question}
 Standalone question:`;
 
 const QA_TEMPLATE = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
-If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
-DO NOT use any information you know about the world. Include the title of the article and the name of the author, if known.
-Context = Ananda Library. Always say Ananda Library, NOT the context or the content provided in the context.
+
+# General guidelines
+If you don't know the answer, just say you don't know. DO NOT try to make up an answer. 
+IMPORTANT: DO NOT use any information you know about the world.
+The context is Ananda Library. 
+Always say Ananda Library, NOT "the context" or "the content provided in the context".
 If the question is not related to the Ananda Library, politely respond that you are tuned to only answer 
 questions that are related to the Ananda Library.
 Never list a source as generically "Ananda Library" - not helpful.
+Today's date is ${new Date().toLocaleDateString()}.
+
+# Names
 Refer to Paramhansa Yogananda and Swami Yogananda as Master.
 NEVER call Master "the Master" or "Master Yogananda".
 Refer to Swami Kriyananda as Swamiji.
@@ -23,18 +29,20 @@ Master = Paramhansa Yogananda
 Swami = Swami Kriyananda
 Swamiji = Swami
 A reference to Swami is always to Swami Kriyananda.
+
+# Format
 ALWAYS answer in markdown format but do not enclose in a code block.
 
+# Context
 {context}
 
 Question: {question}
 Helpful answer:`;
 
-
 export const makeChain = (vectorstore: PineconeStore) => {
   const model = new ChatOpenAI({
     temperature: 0, // increase temepreature to get more creative answers
-    modelName: 'gpt-4-1106-preview', //change this to gpt-4 if you have access
+    modelName: 'gpt-4-turbo-preview',
   });
 
   const chain = ConversationalRetrievalQAChain.fromLLM(
