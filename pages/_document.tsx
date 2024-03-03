6ed4f0b93d83317b80770ab6451b15f1e64fa017
Michael Olivier <lists2@baac.net>
2024-03-03 09:53:53 -0800

    add google tag mgr code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,19 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-0DGBYKZ9LS');
+          `,
+          }}
+        />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-0DGBYKZ9LS"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
