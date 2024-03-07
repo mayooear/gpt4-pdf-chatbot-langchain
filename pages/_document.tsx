261c6427ee5bf76737e1a23d5a50bc0af0166abf
Michael Olivier <lists2@baac.net>
2024-03-07 08:46:36 -0800

    update how we do google tag code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,20 +1,27 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <script
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-0DGBYKZ9LS`}
+        />
+        <Script
+          id="google-analytics-script"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-0DGBYKZ9LS');
-          `,
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-0DGBYKZ9LS');
+            `,
           }}
         />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-0DGBYKZ9LS"></script>
       </Head>
       <body>
         <Main />
