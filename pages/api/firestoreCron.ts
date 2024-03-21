2430a1d0f743594c482537ac5d89f9ca5d32aedc
Michael Olivier <lists2@baac.net>
2024-03-20 19:55:54 -0700

    try to defeat cold start problem with firestore

---
+++ pages/api/firestoreCron.ts
@@ -0,0 +1,11 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import firebase from 'firebase-admin';
+import { db } from '@/services/firebase'; 
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    // Make any query to avoid cold start problem
+    const snapshot = db.collection(`${process.env.ENVIRONMENT}_chatLogs`)
+                      .where(firebase.firestore.FieldPath.documentId(), 'in', ['000000'])
+                      .get();
+    res.status(200).end('Done');
+}
