c2a938a52a6eec48a14b833f6607233bbbab0b1e
Michael Olivier <lists2@baac.net>
2024-03-30 07:56:45 -0700

    improve messaging

--- ananda-doc-gen/filter-pdfs-to-new-dir.py
+++ ananda-doc-gen/filter-pdfs-to-new-dir.py
@@ -18,6 +18,10 @@ def duplicate_pdf_files(source_dir, dest_dir, authors):
         if proceed.lower() != 'y':
             print("Operation cancelled.")
             return
+
+    author_mismatch_count = 0
+    missing_author_count = 0
+
     for filename in tqdm(os.listdir(source_dir)):
         if filename.endswith(".pdf"):
             pdf_file_path = os.path.join(source_dir, filename)
@@ -29,17 +33,18 @@ def duplicate_pdf_files(source_dir, dest_dir, authors):
                 continue
             metadata = pdf_reader.metadata
             author = metadata.get('/Author', None or '')
-            # print("author: " + author)
-            # continue
 
             if author:
                 if any(name.lower() in author.lower() for name in authors):
                     shutil.copy(pdf_file_path, dest_dir)
                 else:
-                    print(f"Filtered out file {pdf_file_path} due to author mismatch.")
+                    author_mismatch_count += 1
             else:
-                print(f"Filtered out file {pdf_file_path} due to missing author.")
+                missing_author_count += 1
             pdf_file_obj.close()
+
+    print(f"Total files filtered out due to author mismatch: {author_mismatch_count}")
+    print(f"Total files filtered out due to missing author: {missing_author_count}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
