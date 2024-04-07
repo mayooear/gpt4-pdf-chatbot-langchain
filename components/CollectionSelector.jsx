f6c23a57586e40248be20052dfc0d670c45d1de3
Michael Olivier <lists2@baac.net>
2024-04-06 18:01:56 -0700

    reduce size of dropdown

--- components/CollectionSelector.jsx
+++ components/CollectionSelector.jsx
@@ -5,7 +5,7 @@ export default function CollectionSelector({ onCollectionChange, currentCollecti
         id="collection"
         value={currentCollection}
         onChange={(e) => onCollectionChange(e.target.value)}
-        className="block w-full pl-3 pr-10 py-3 text-lg border-gray-300 
+        className="block w-full pl-3 pr-10 py-2 text-base border-gray-300 
         focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 rounded-md"
       >
         <option value="master_swami">Master and Swamiji Only</option>
