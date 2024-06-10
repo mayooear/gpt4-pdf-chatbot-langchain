952f540965b0d4bfcc85bd4ef90395185d01a9fb
Michael Olivier <lists2@baac.net>
2024-06-09 18:49:02 -0700

    Temporarily turn off redirect to login

--- middleware.ts
+++ middleware.ts
@@ -5,20 +5,20 @@ export function middleware(req: NextRequest) {
   const url = req.nextUrl.clone();
   const cookie = req.cookies.get('siteAuth');
 
-  if (
-    !cookie &&
-    url.pathname !== '/login' &&
-    !url.pathname.startsWith('/_next') &&
-    !url.pathname.startsWith('/api') &&
-    !url.pathname.startsWith('/favicon.ico') &&
-    !url.pathname.endsWith('.png') &&
-    !url.pathname.endsWith('.jpg') &&
-    !url.pathname.endsWith('.gif')
-  ) {
-    url.pathname = '/login';
-    url.searchParams.set('redirect', req.nextUrl.pathname);
-    return NextResponse.redirect(url);
-  }
+  // if (
+  //   !cookie &&
+  //   url.pathname !== '/login' &&
+  //   !url.pathname.startsWith('/_next') &&
+  //   !url.pathname.startsWith('/api') &&
+  //   !url.pathname.startsWith('/favicon.ico') &&
+  //   !url.pathname.endsWith('.png') &&
+  //   !url.pathname.endsWith('.jpg') &&
+  //   !url.pathname.endsWith('.gif')
+  // ) {
+  //   url.pathname = '/login';
+  //   url.searchParams.set('redirect', req.nextUrl.pathname);
+  //   return NextResponse.redirect(url);
+  // }
 
   return NextResponse.next();
 }
