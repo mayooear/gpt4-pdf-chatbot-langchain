c2204226d3d62aca050c926abce2f928e4767356
Michael Olivier <lists2@baac.net>
2024-06-10 13:10:12 -0700

    Turn on forcing login

--- middleware.ts
+++ middleware.ts
@@ -11,21 +11,21 @@ export function middleware(req: NextRequest) {
   }
 
   // Authentication check
-  // const cookie = req.cookies.get('siteAuth');
-  // if (
-  //   !cookie &&
-  //   url.pathname !== '/login' &&
-  //   !url.pathname.startsWith('/_next') &&
-  //   !url.pathname.startsWith('/api') &&
-  //   !url.pathname.startsWith('/favicon.ico') &&
-  //   !url.pathname.endsWith('.png') &&
-  //   !url.pathname.endsWith('.jpg') &&
-  //   !url.pathname.endsWith('.gif')
-  // ) {
-  //   url.pathname = '/login';
-  //   url.searchParams.set('redirect', req.nextUrl.pathname);
-  //   return NextResponse.redirect(url);
-  // }
+  const cookie = req.cookies.get('siteAuth');
+  if (
+    !cookie &&
+    url.pathname !== '/login' &&
+    !url.pathname.startsWith('/_next') &&
+    !url.pathname.startsWith('/api') &&
+    !url.pathname.startsWith('/favicon.ico') &&
+    !url.pathname.endsWith('.png') &&
+    !url.pathname.endsWith('.jpg') &&
+    !url.pathname.endsWith('.gif')
+  ) {
+    url.pathname = '/login';
+    url.searchParams.set('redirect', req.nextUrl.pathname);
+    return NextResponse.redirect(url);
+  }
 
   return NextResponse.next();
 }
