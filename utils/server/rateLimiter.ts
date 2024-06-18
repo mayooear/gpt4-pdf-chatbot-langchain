6828d8f67e50c231136a55df0453bcf987f5ba60
Michael Olivier <lists2@baac.net>
2024-06-17 19:56:57 -0700

    fail gracefully

--- utils/server/rateLimiter.ts
+++ utils/server/rateLimiter.ts
@@ -12,35 +12,45 @@ export async function rateLimiter(req: NextApiRequest, res: NextApiResponse): Pr
   // sharing one collection across dev and prod
   const rateLimitRef = db.collection('rateLimits').doc(ip);
 
-  const rateLimitDoc = await rateLimitRef.get();
-  if (!rateLimitDoc.exists) {
-    await rateLimitRef.set({
-      count: 1,
-      firstRequestTime: now,
-    });
-    return true;
-  }
-
-  const rateLimitData = rateLimitDoc.data();
-  if (rateLimitData) {
-    const { count, firstRequestTime } = rateLimitData;
-    if (now - firstRequestTime < RATE_LIMIT_WINDOW_MS) {
-        await rateLimitRef.update({
-            count: count + 1,
-          });
-        if (count >= MAX_REQUESTS) {
-            res.status(429).json({ message: 'Too many attempts. Please try again later.' });
-            return false;
-        } else {
-            return true;
-        }
-    } else {
+  try {
+    const rateLimitDoc = await rateLimitRef.get();
+    if (!rateLimitDoc.exists) {
       await rateLimitRef.set({
         count: 1,
         firstRequestTime: now,
       });
       return true;
     }
+
+    const rateLimitData = rateLimitDoc.data();
+    if (rateLimitData) {
+        const { count, firstRequestTime } = rateLimitData;
+        if (now - firstRequestTime < RATE_LIMIT_WINDOW_MS) {
+            await rateLimitRef.update({
+                count: count + 1,
+            });
+            if (count >= MAX_REQUESTS) {
+                res.status(429).json({ message: 'Too many attempts. Please try again later.' });
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+        await rateLimitRef.set({
+            count: 1,
+            firstRequestTime: now,
+        });
+        return true;
+        }
+    }
+    return true;
+    
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error('FetchError:', error.message);
+    } else {
+      console.error('FetchError:', error);
+    }
+    return true;
   }
-  return true;
 }
