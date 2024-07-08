086fe715704737d184f47649d31669916af37033
Michael Olivier <lists2@baac.net>
2024-07-08 15:36:20 -0700

    Use accordion version of sources

--- pages/answers.tsx
+++ pages/answers.tsx
@@ -332,7 +332,7 @@ const AllAnswers = () => {
                     <div className="markdownanswer">
                       <TruncatedMarkdown markdown={answer.answer} maxCharacters={600} />
                       {answer.sources && (
-                        <SourcesList sources={answer.sources} useAccordion={true} />
+                        <SourcesList sources={answer.sources} useAccordion={false} />
                       )}
                       <div className="flex items-center">
                         <CopyButton
