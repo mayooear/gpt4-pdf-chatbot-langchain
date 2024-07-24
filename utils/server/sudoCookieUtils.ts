f3ece4661fc9eb66a23dd4ba6dde8ca5f0cc65e4
Michael Olivier <lists2@baac.net>
2024-07-24 14:27:02 -0700

    Add some debugging and safety checks

--- utils/server/sudoCookieUtils.ts
+++ utils/server/sudoCookieUtils.ts
@@ -15,13 +15,18 @@ function encrypt(text: string) {
 }
 
 function decrypt(text: string) {
-  const textParts = text.split(':');
-  const iv = Buffer.from(textParts.shift()!, 'hex');
-  const encryptedText = Buffer.from(textParts.join(':'), 'hex');
-  const decipher = crypto.createDecipheriv('aes-256-cbc', secretKey, iv);
-  let decrypted = decipher.update(encryptedText);
-  decrypted = Buffer.concat([decrypted, decipher.final()]);
-  return decrypted.toString();
+  try {
+    const textParts = text.split(':');
+    const iv = Buffer.from(textParts.shift()!, 'hex');
+    const encryptedText = Buffer.from(textParts.join(':'), 'hex');
+    const decipher = crypto.createDecipheriv('aes-256-cbc', secretKey, iv);
+    let decrypted = decipher.update(encryptedText);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return decrypted.toString();
+  } catch (error) {
+    console.error('Decryption error:', error);
+    throw new Error('Decryption failed');
+  }
 }
 
 async function setSudoCookie(req: NextApiRequest, res: NextApiResponse, password: string) {
@@ -64,16 +69,24 @@ function getSudoCookie(req: NextApiRequest, res: NextApiResponse) {
 
   if (encryptedToken) {
     try {
+      const textParts = encryptedToken.split(':');
+      if (textParts.length !== 2) {
+        throw new Error('Invalid token format');
+      }
       const decryptedToken = decrypt(encryptedToken);
       const tokenIndex = decryptedToken.indexOf(':');
       const token = decryptedToken.slice(0, tokenIndex);
       const ip = decryptedToken.slice(tokenIndex + 1);
+      console.log('Decrypted token:', token);
+      console.log('Extracted IP:', ip);
+      console.log('User IP:', userIp);
       if (ip === userIp) {
         return { sudoCookieValue: true };
       } else {
         throw new Error('Invalid token or IP mismatch');
       }
     } catch (error) {
+      console.error('Token validation error:', error);
       throw new Error('Invalid token');
     }
   } else {
