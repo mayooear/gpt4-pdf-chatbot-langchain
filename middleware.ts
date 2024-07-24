9e71332d89adc562d1fd45c42eadc4ab60538abc
Michael Olivier <lists2@baac.net>
2024-07-24 10:15:04 -0700

    Rollback to previous login implementation

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import CryptoJS from 'crypto-js';
 import { isDevelopment } from '@/utils/env';
 
 export function middleware(req: NextRequest) {
@@ -18,7 +17,10 @@ export function middleware(req: NextRequest) {
     return NextResponse.redirect(url);
   }
 
-  const pathname_is_private = (
+  // Authentication check
+  const cookie = req.cookies.get('siteAuth');
+  if (
+    !cookie &&
     url.pathname !== '/login' &&
     !(url.pathname.startsWith('/answers/') && url.pathname !== '/answers/') && 
     !url.pathname.startsWith('/_next') &&
@@ -28,29 +30,10 @@ export function middleware(req: NextRequest) {
     !url.pathname.endsWith('.png') &&
     !url.pathname.endsWith('.jpg') &&
     !url.pathname.endsWith('.gif')
-  );
-
-  if (pathname_is_private) {
-    // Authentication check
-    const cookie = req.cookies.get('siteAuth');
-    const storedHashedToken = process.env.SECURE_TOKEN_HASH;
-    if (!cookie || CryptoJS.SHA256(cookie.value).toString() !== storedHashedToken) {
-      console.log('Authentication failed');
-      
-      // For API routes, return a 401 Unauthorized response
-      if (url.pathname.startsWith('/api')) {
-        return new NextResponse(
-          JSON.stringify({ success: false, message: 'Authentication required' }),
-          { status: 401, headers: { 'content-type': 'application/json' } }
-        );
-      }
-      
-      // For other routes, redirect to login
-      console.log('Redirecting to /login');
-      url.pathname = '/login';
-      url.searchParams.set('redirect', req.nextUrl.pathname);
-      return NextResponse.redirect(url);
-    }
+  ) {
+    url.pathname = '/login';
+    url.searchParams.set('redirect', req.nextUrl.pathname);
+    return NextResponse.redirect(url);
   }
 
   return NextResponse.next();
--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,7 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import bcrypt from 'bcrypt';
 import Cookies from 'cookies';
-import crypto from 'crypto';
 import cors, { runMiddleware } from 'utils/server/corsMiddleware';
 import { rateLimiter } from 'utils/server/rateLimiter';
 import { isDevelopment } from '@/utils/env';
@@ -18,42 +17,24 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return;
   }
 
+  console.log(`Request method: ${req.method}`);
+
   if (req.method === 'POST') {
     const { password, redirect } = req.body;
     const storedHashedPassword = process.env.SITE_PASSWORD;
-    const storedHashedToken = process.env.SECURE_TOKEN_HASH;
-    const fixedToken = process.env.SECURE_TOKEN;
 
-    if (!password || !storedHashedPassword || !storedHashedToken || !fixedToken) {
+    if (!password || !storedHashedPassword) {
       return res.status(400).json({ message: 'Bad request' });
     }
 
     const match = await bcrypt.compare(password, storedHashedPassword);
     if (match) {
-      const hashedToken = crypto.createHash('sha256').update(fixedToken).digest('hex');
-
-      if (hashedToken !== storedHashedToken) {
-        return res.status(500).json({ message: 'Server error' });
-      }
-
       const isSecure = req.headers['x-forwarded-proto'] === 'https' || !isDevelopment();
       const cookies = new Cookies(req, res, { secure: isSecure });
 
       const expiryDate = new Date();
       expiryDate.setMonth(expiryDate.getMonth() + 12);
-      cookies.set('siteAuth', fixedToken, {
-        httpOnly: true,
-        secure: isSecure,
-        expires: expiryDate,
-        sameSite: 'lax',
-      });
-      // Less secure cookie that can be used for some things on the front end
-      cookies.set('isLoggedIn', 'true', {
-        httpOnly: false,
-        secure: isSecure,
-        expires: expiryDate,
-        sameSite: 'lax',
-      });
+      cookies.set('siteAuth', 'true', { httpOnly: true, secure: isSecure, expires: expiryDate });
       return res.status(200).json({ message: 'Authenticated', redirect });
     } else {
       return res.status(403).json({ message: 'Incorrect password' });
