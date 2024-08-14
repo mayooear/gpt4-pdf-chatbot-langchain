9ef0898f964419b2ccff23397c64dd243860879c
Michael Olivier <lists2@baac.net>
2024-08-14 10:35:51 -0700

    minor tweaks

--- components/layout.tsx
+++ components/layout.tsx
@@ -43,27 +43,52 @@ export default function Layout({ children }: LayoutProps) {
         <div className="h-16 border-b border-b-slate-200 py-4 flex justify-between items-center px-4">
           <nav className="ml-2 pl-1">
             <div className="space-x-10">
-              <Link href="https://www.anandalibrary.org/" className="text-sm text-gray-500 hover:text-slate-600 cursor-pointer" onClick={handleBackToLibrary}>
-                ← Back to Ananda Library
+              <Link
+                href="https://www.anandalibrary.org/"
+                className="text-sm text-gray-500 hover:text-slate-600 cursor-pointer"
+                onClick={handleBackToLibrary}
+              >
+                ← Ananda Library
               </Link>
-              <Link href="/" className={`hover:text-slate-600 cursor-pointer ${isActive('/') ? 'text-slate-800 font-bold' : ''}`}>
+              <Link
+                href="/"
+                className={`hover:text-slate-600 cursor-pointer ${
+                  isActive('/') ? 'text-slate-800 font-bold' : ''
+                }`}
+              >
                 Ask
               </Link>
-              <Link href="/answers" className={`hover:text-slate-600 cursor-pointer ${isActive('/answers') ? 'text-slate-800 font-bold' : ''}`}>
+              <Link
+                href="/answers"
+                className={`hover:text-slate-600 cursor-pointer ${
+                  isActive('/answers') ? 'text-slate-800 font-bold' : ''
+                }`}
+              >
                 All&nbsp;Answers
               </Link>
             </div>
           </nav>
           <nav className="mr-4 pr-6 flex space-x-4">
-            <a href="https://www.anandalibrary.org/content/ai-chatbot-intro/" className="hover:text-slate-600 cursor-pointer" onClick={handleHelp}>
+            <a
+              href="https://www.anandalibrary.org/content/ai-chatbot-intro/"
+              className="text-sm text-gray-500 hover:text-slate-600 cursor-pointer"
+              onClick={handleHelp}
+            >
               Help
             </a>
             {isLoggedIn ? (
-              <a href="#" onClick={handleLogout} className="hover:text-slate-600 cursor-pointer">
+              <a
+                href="#"
+                onClick={handleLogout}
+                className="text-sm text-gray-500 hover:text-slate-600 cursor-pointer"
+              >
                 Logout
               </a>
             ) : (
-              <Link href="/login" className="hover:text-slate-600 cursor-pointer">
+              <Link
+                href="/login"
+                className="text-sm text-gray-500 hover:text-slate-600 cursor-pointer"
+              >
                 Login
               </Link>
             )}
@@ -71,11 +96,9 @@ export default function Layout({ children }: LayoutProps) {
         </div>
       </header>
       <div className="flex-grow">
-        <main className="flex flex-1 flex-col overflow-hidden">
-          {children}
-        </main>
+        <main className="flex flex-1 flex-col overflow-hidden">{children}</main>
       </div>
-      <Footer /> 
+      <Footer />
     </div>
   );
 }
