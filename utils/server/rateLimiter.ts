eca8713e603473ddc76e4ae4d883b6021f848cb7
Michael Olivier <lists2@baac.net>
2024-08-27 11:48:24 -0700

    File formatting only changed to force a rebuild

--- utils/server/rateLimiter.ts
+++ utils/server/rateLimiter.ts
@@ -6,9 +6,15 @@ import { isDevelopment } from '@/utils/env';
 const RATE_LIMIT_WINDOW_MS = isDevelopment() ? 5 * 60 * 1000 : 15 * 60 * 1000;
 const MAX_REQUESTS = 8;
 
-// Rate limiting by IP address. All uses of this contribute to counts against IP addresses. 
-export async function rateLimiter(req: NextApiRequest, res: NextApiResponse): Promise<boolean> {
-  const ip = req.headers['x-forwarded-for']?.toString() || req.socket.remoteAddress || '127.0.0.1';
+// Rate limiting by IP address. All uses of this contribute to counts against IP addresses.
+export async function rateLimiter(
+  req: NextApiRequest,
+  res: NextApiResponse,
+): Promise<boolean> {
+  const ip =
+    req.headers['x-forwarded-for']?.toString() ||
+    req.socket.remoteAddress ||
+    '127.0.0.1';
   const now = Date.now();
 
   // sharing one collection across dev and prod
@@ -26,29 +32,32 @@ export async function rateLimiter(req: NextApiRequest, res: NextApiResponse): Pr
 
     const rateLimitData = rateLimitDoc.data();
     if (rateLimitData) {
-        const { count, firstRequestTime } = rateLimitData;
-        if (now - firstRequestTime < RATE_LIMIT_WINDOW_MS) {
-            await rateLimitRef.update({
-                count: count + 1,
-            });
-            if (count >= MAX_REQUESTS) {
-                const formattedTime = new Date(firstRequestTime).toLocaleString();
-                console.log(`rate limiter: IP ${ip} has made ${count} attempts since ${formattedTime}`);
-                res.status(429).json({ message: 'Too many attempts. Please try again later.' });
-                return false;
-            } else {
-                return true;
-            }
+      const { count, firstRequestTime } = rateLimitData;
+      if (now - firstRequestTime < RATE_LIMIT_WINDOW_MS) {
+        await rateLimitRef.update({
+          count: count + 1,
+        });
+        if (count >= MAX_REQUESTS) {
+          const formattedTime = new Date(firstRequestTime).toLocaleString();
+          console.log(
+            `rate limiter: IP ${ip} has made ${count} attempts since ${formattedTime}`,
+          );
+          res
+            .status(429)
+            .json({ message: 'Too many attempts. Please try again later.' });
+          return false;
         } else {
+          return true;
+        }
+      } else {
         await rateLimitRef.set({
-            count: 1,
-            firstRequestTime: now,
+          count: 1,
+          firstRequestTime: now,
         });
         return true;
-        }
+      }
     }
     return true;
-
   } catch (error: unknown) {
     if (error instanceof Error) {
       console.error('FetchError:', error.message);
