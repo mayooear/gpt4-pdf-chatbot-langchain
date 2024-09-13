422a599bd47decc6c0846095f0ba34797b7114ae
Michael Olivier <lists2@baac.net>
2024-09-13 09:56:00 -0700

    CSP Same for development and production

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,10 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || '';
 
-  const cspContent =
-    process.env.NODE_ENV === 'development'
-      ? `default-src 'self' ${baseUrl}; script-src 'self' ${baseUrl} 'unsafe-inline' 'unsafe-eval'; style-src 'self' ${baseUrl} 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' ${baseUrl} https://fonts.gstatic.com data:; img-src 'self' ${baseUrl} data:; connect-src 'self' ${baseUrl}; media-src 'self' ${baseUrl} https://ananda-chatbot.s3.us-west-1.amazonaws.com; frame-src 'self' ${baseUrl} https://www.youtube.com https://www.youtube-nocookie.com https://youtu.be`
-      : `default-src 'self' ${baseUrl}; script-src 'self' ${baseUrl} 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com; connect-src 'self' ${baseUrl} https://www.google-analytics.com https://analytics.google.com https://www.googletagmanager.com; style-src 'self' ${baseUrl} 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' ${baseUrl} https://fonts.gstatic.com data:; img-src 'self' ${baseUrl} https://www.google-analytics.com https://www.googletagmanager.com data:; media-src 'self' ${baseUrl} https://ananda-chatbot.s3.us-west-1.amazonaws.com blob:; frame-src 'self' ${baseUrl} https://www.youtube.com https://www.youtube-nocookie.com https://youtu.be`;
+  const cspContent = `default-src 'self' ${baseUrl};
+    script-src 'self' ${baseUrl} 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com;
+    connect-src 'self' ${baseUrl} https://www.google-analytics.com https://analytics.google.com https://www.googletagmanager.com;
+    style-src 'self' ${baseUrl} 'unsafe-inline' https://fonts.googleapis.com;
+    font-src 'self' ${baseUrl} https://fonts.gstatic.com data:;
+    img-src 'self' ${baseUrl} https://www.google-analytics.com https://www.googletagmanager.com data:;
+    media-src 'self' ${baseUrl} https://ananda-chatbot.s3.us-west-1.amazonaws.com blob:;
+    frame-src 'self' ${baseUrl} https://www.youtube.com https://www.youtube-nocookie.com https://youtu.be`;
 
   return (
     <Html lang="en">
