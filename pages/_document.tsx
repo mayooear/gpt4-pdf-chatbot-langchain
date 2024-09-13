0347029bb20da85df5cfa430c69a8fc65cc32c12
Michael Olivier <lists2@baac.net>
2024-09-13 14:21:20 -0700

    Changes to not block JavaScript evaluation in Google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,14 +3,16 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || '';
 
-  const cspContent = `default-src 'self' ${baseUrl};
-    script-src 'self' ${baseUrl} 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com;
+  const cspContent = `
+    default-src 'self' ${baseUrl};
+    script-src 'self' ${baseUrl} 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com;
     connect-src 'self' ${baseUrl} https://www.google-analytics.com https://analytics.google.com https://www.googletagmanager.com;
     style-src 'self' ${baseUrl} 'unsafe-inline' https://fonts.googleapis.com;
     font-src 'self' ${baseUrl} https://fonts.gstatic.com data:;
     img-src 'self' ${baseUrl} https://www.google-analytics.com https://www.googletagmanager.com data:;
     media-src 'self' ${baseUrl} https://ananda-chatbot.s3.us-west-1.amazonaws.com blob:;
-    frame-src 'self' ${baseUrl} https://www.youtube.com https://www.youtube-nocookie.com https://youtu.be`;
+    frame-src 'self' ${baseUrl} https://www.youtube.com https://www.youtube-nocookie.com https://youtu.be;
+  `;
 
   return (
     <Html lang="en">
