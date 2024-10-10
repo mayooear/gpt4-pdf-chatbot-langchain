da2c63519d8354fe27e020033165d31312a6e20d
Michael Olivier <lists2@baac.net>
2024-10-09 17:34:39 -0700

    Make sure toast isn't hidden behind the survey icon. Move it to top.

--- components/Toast.tsx
+++ components/Toast.tsx
@@ -15,7 +15,21 @@ const Toast: React.FC<ToastProps> = ({ message, onClose }) => {
   }, [onClose]);
 
   return (
-    <div className="fixed bottom-4 right-4 bg-green-500 text-white px-4 py-2 rounded shadow-lg">
+    <div className="fixed top-4 left-1/2 transform -translate-x-1/2 bg-green-500 text-white px-4 py-2 rounded shadow-lg flex items-center z-50">
+      <svg
+        className="w-5 h-5 mr-2"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M5 13l4 4L19 7"
+        />
+      </svg>
       {message}
     </div>
   );
