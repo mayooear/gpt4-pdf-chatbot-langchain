59fcbb6d6ca4b89334955b2625d2ccef751195e6
Michael Olivier <lists2@baac.net>
2024-10-20 11:39:52 -0700

    fix for preview:
    This is blocking me from logging in in preview of production
    
    Suspicious POST request to /api/login: Missing or invalid referer. IP: 127.0.0.1

--- utils/server/apiMiddleware.ts
+++ utils/server/apiMiddleware.ts
@@ -4,21 +4,33 @@ type ApiHandler = (req: NextApiRequest, res: NextApiResponse) => Promise<void>;
 
 export function withApiMiddleware(handler: ApiHandler): ApiHandler {
   return async (req: NextApiRequest, res: NextApiResponse) => {
-    // Check for missing or suspicious referer
     const referer = req.headers.referer || req.headers.referrer;
-    if (
-      req.method === 'POST' &&
-      (!referer ||
-        (typeof referer === 'string' &&
-          !referer.startsWith(process.env.NEXT_PUBLIC_BASE_URL || '')))
-    ) {
-      console.warn(
-        `Suspicious POST request to ${req.url}: Missing or invalid referer. IP: ${req.socket.remoteAddress}`,
-      );
-      return res.status(403).json({ message: 'Forbidden: Invalid referer' });
-    }
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || '';
+    const isVercelPreview = process.env.VERCEL_ENV === 'preview';
+    const isDevelopment = process.env.NODE_ENV === 'development';
 
-    // Add more common checks here as needed
+    if (req.method === 'POST' && !isDevelopment) {
+      if (!referer) {
+        console.warn(
+          `Suspicious POST request to ${req.url}: Missing referer. IP: ${req.socket.remoteAddress}`,
+        );
+        return res.status(403).json({ message: 'Forbidden: Missing referer' });
+      }
+
+      if (typeof referer === 'string') {
+        const refererUrl = new URL(referer);
+        const baseUrlObj = new URL(baseUrl);
+
+        if (!isVercelPreview && refererUrl.hostname !== baseUrlObj.hostname) {
+          console.warn(
+            `Suspicious POST request to ${req.url}: Invalid referer. IP: ${req.socket.remoteAddress}, Referer: ${referer}`,
+          );
+          return res
+            .status(403)
+            .json({ message: 'Forbidden: Invalid referer' });
+        }
+      }
+    }
 
     // If all checks pass, call the original handler
     await handler(req, res);
