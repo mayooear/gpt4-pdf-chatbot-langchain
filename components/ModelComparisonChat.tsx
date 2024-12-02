f8e810c196a2a28f378b5f6d26621524bf904238
Michael Olivier <lists2@baac.net>
2024-12-01 16:51:48 -0800

    Randomize on reset

--- components/ModelComparisonChat.tsx
+++ components/ModelComparisonChat.tsx
@@ -471,7 +471,10 @@ const ModelComparisonChat: React.FC<ModelComparisonChatProps> = ({
         </button>
         {conversationStarted && (
           <button
-            onClick={handleReset}
+            onClick={() => {
+              handleReset();
+              handleRandomize();
+            }}
             className="px-4 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-600 transition-colors w-fit"
           >
             Reset
@@ -621,7 +624,6 @@ const ModelComparisonChat: React.FC<ModelComparisonChatProps> = ({
                 onClick={() => {
                   setShowThankYouMessage(false);
                   handleReset();
-                  handleRandomize();
                 }}
                 className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors"
               >
