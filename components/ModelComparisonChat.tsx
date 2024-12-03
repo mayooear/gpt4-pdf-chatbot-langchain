dcba84a6b84ec7f5c607ffca42f1888c8a21e190
Michael Olivier <lists2@baac.net>
2024-12-02 17:17:52 -0800

    Fixed bug where it was doing two entries instead of one in the database

--- components/ModelComparisonChat.tsx
+++ components/ModelComparisonChat.tsx
@@ -714,7 +714,8 @@ const ModelComparisonChat: React.FC<ModelComparisonChatProps> = ({
               <button
                 onClick={() => {
                   setShowThankYouMessage(false);
-                  handleSkipAndNew();
+                  handleReset();
+                  handleRandomize();
                 }}
                 className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors"
               >
