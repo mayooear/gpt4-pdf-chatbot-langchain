42a3b5ce919e76e0611d59ca8eb1a8248010d051
Michael Olivier <lists2@baac.net>
2025-01-09 18:21:35 -0800

    UI correction

--- components/NPSSurvey.tsx
+++ components/NPSSurvey.tsx
@@ -241,7 +241,7 @@ const NPSSurvey: React.FC<NPSSurveyProps> = ({
                 maxLength={1000}
               />
               {/* Additional comments textarea */}
-              <h3 className="text-base font-semibold mb-2">
+              <h3 className="text-base font-medium mb-2">
                 What would make it even better? Or other comments (optional).
               </h3>
               <textarea
